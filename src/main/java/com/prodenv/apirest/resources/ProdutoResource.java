78f233893f23c6f851977e355401e1d317240ec7
gerson36 <gfb@cin.ufpe.br>
2021-03-14 22:58:22 -0300

    Atualização

--- src/main/java/com/prodenv/apirest/resources/ProdutoResource.java
+++ src/main/java/com/prodenv/apirest/resources/ProdutoResource.java
@@ -3,6 +3,7 @@ package com.prodenv.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,17 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prodenv.apirest.repository.ProdutoRepository;
+
+import io.swagger.annotations.Api;
+
 import com.prodenv.apirest.models.Produto;
 
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Produtos")
+@CrossOrigin(origins="*")
 public class ProdutoResource {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
 	@GetMapping("/produtos")
-	public List<Produto> listaProdutos(){
+		public List<Produto> listaProdutos(){
 		return produtoRepository.findAll();
 	}
 	
